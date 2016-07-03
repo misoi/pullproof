c2fd9532bed1bded98e9eba33ad5da3ff6de95e4
Davies Tonui <tonuidavies@gmail.com>
2016-07-03 14:04:16 +0300

    my commits

---
+++ project/home/views.py
@@ -0,0 +1,36 @@
+#################
+#### imports ####
+#################
+
+from flask import render_template, Blueprint
+from flask.ext.login import login_required
+
+from project import db
+from project.models import BlogPost
+
+################
+#### config ####
+################
+
+home_blueprint = Blueprint(
+    'home', __name__,
+    template_folder='templates'
+)
+
+
+################
+#### routes ####
+################
+
+# use decorators to link the function to a url
+@home_blueprint.route('/')
+@login_required
+def home():
+    # return "Hello, World!"  # return a string
+    posts = db.session.query(BlogPost).all()
+    return render_template('index.html', posts=posts)  # render a template
+
+
+@home_blueprint.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
